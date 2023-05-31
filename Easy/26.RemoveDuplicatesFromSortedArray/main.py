3a1c7a157ce9e13bba71baa236cf639f625d0fc4
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-05-31 14:30:16 +0300

    Доделал 26 задачу

--- Easy/26.RemoveDuplicatesFromSortedArray/main.py
+++ Easy/26.RemoveDuplicatesFromSortedArray/main.py
@@ -1,74 +1,29 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-    def __str__(self) -> str:
-        return str(self.val)
-    @classmethod
-    def get_head_list(cls, mass):
-        node_head = None
-        last_node = None
-        for x in mass:
-            node = cls(x)
-            if node_head is None:
-                node_head = node
-            else:
-                last_node.next = node
-            last_node = node
-            
-        return node_head
-    @classmethod
-    def check_2_listnode(cls, ls1, ls2):
-        while ls1 and ls2:
-            if ls1.val != ls2.val:
-                return False
-            ls1 = ls1.next
-            ls2 = ls2.next
-        return ls1 is None and ls2 is None
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
+    def removeDuplicates(self, nums):
         """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
+        :type nums: List[int]
+        :rtype: int
         """
-        node_head = None
-        last_node = None
-        while list1 is not None and list2 is not None:
-            if list1.val < list2.val:
-                node = ListNode(val=list1.val)
-                if node_head is None:
-                    node_head = node
-                else:
-                    last_node.next  = node
-                list1 = list1.next
-                last_node = node
-
-            else:
-                node = ListNode(val=list2.val)
-                if node_head is None:
-                    node_head = node
-                else:
-                    last_node.next  =  node
-                list2 = list2.next
-                last_node = node
-        for list in [list1, list2]:
-            while list:
-                node = ListNode(val=list.val)
-                if node_head is None:
-                    node_head = node
-                else:
-                    last_node.next  = node
-                list = list.next
-                last_node = node
-        return node_head
-
+        k =0 
+        i =0
+        last_x = None
+        while i< len(nums):
+            x = nums[i]
+            if last_x is None:
+                last_x = x
+                k += 1
+                i += 1
+                continue
+            if x != last_x:
+                nums[k],nums[i] = nums[i], nums[k]
+                last_x = x
+                k += 1
+            i += 1
+        return k
 
 
 if __name__ == "__main__":
     s = Solution()
   
-    assert ListNode.check_2_listnode(s.mergeTwoLists(ListNode.get_head_list([1,2,4]), ListNode.get_head_list([1, 3, 4])), ListNode.get_head_list([1,1,2,3,4,4]) )
-    assert s.mergeTwoLists(None, None) == None
-    assert not s.mergeTwoLists(None, ListNode(0)) == ListNode(0)
+    assert s.removeDuplicates([1,1,2]) == 2
+    assert s.removeDuplicates([0,0,1,1,1,2,2,3,3,4]) == 5
--- Easy/README.md
+++ Easy/README.md
@@ -1,8 +1,9 @@
-|         Номер по порядку         |         Название               | Статус решения           |
-| :----------------------------:   | :----------------------------: | :----------------------: |  
-|1                                 | 1. TwoSum                      | Accepted                 |
-|2                                 | 9. PalindromeNumber            | Accepted                 |
-|3                                 | 13. RomanToInteger             | Accepted                 |
-|4                                 | 14. Longest Common Prefix      | Accepted                 |  
-|5                                 | 20. ValidParentheses           | Accepted                 |  
-|6                                 | 21. Merge Two Sorted Lists     | Accepted                 |   
+|         Номер по порядку         |         Название                       | Статус решения           |
+| :----------------------------:   | :------------------------------------: | :----------------------: |  
+|1                                 | 1. TwoSum                              | Accepted                 |
+|2                                 | 9. PalindromeNumber                    | Accepted                 |
+|3                                 | 13. RomanToInteger                     | Accepted                 |
+|4                                 | 14. Longest Common Prefix              | Accepted                 |  
+|5                                 | 20. ValidParentheses                   | Accepted                 |  
+|6                                 | 21. Merge Two Sorted Lists             | Accepted                 |   
+|7                                 | 26.RemoveDuplicatesFromSortedArray     | Accepted                 | 
