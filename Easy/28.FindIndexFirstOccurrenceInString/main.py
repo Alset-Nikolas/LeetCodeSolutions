d9d4ccd3a58b5c7e2a18cade2bd9f062e4a2c14d
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-05-31 15:19:19 +0300

    Доделал 28 задачу

---
+++ Easy/28.FindIndexFirstOccurrenceInString/main.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        # try:
+        #     return haystack.index(needle)
+        # except ValueError:
+        #     return -1
+    
+        for i in range(len(haystack)-len(needle)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    assert s.strStr('sadbutsad', 'sad') == 0
+    assert s.strStr('leetcode', 'leeto') == -1
+    assert s.strStr('mississippi', 'issip') == 4
