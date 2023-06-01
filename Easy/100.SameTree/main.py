f3e887b5048158ee0671c3140d2d43216501432b
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-01 23:25:43 +0300

    Дописал 100 задачу

--- Easy/100.SameTree/main.py
+++ Easy/100.SameTree/main.py
@@ -5,20 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def inorderTraversal(self, root):
+    def isSameTree(self, p, q):
         """
-        :type root: TreeNode
-        :rtype: List[int]
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
         """
-        val_items = []
-        left_ch = []
-        right_ch = []
-        node = root
-        if node:
-            val_items.append(node.val)
-            if node.left:
-                left_ch = self.inorderTraversal(node.left)
-            if node.right:
-                right_ch = self.inorderTraversal(node.right)
-        return left_ch + val_items  + right_ch
+        if bool(p) + bool(q) == 1:
+            return False
+        if not (p and q):
+            return True
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
--- Easy/94.BinaryTreeInorderTraversal/main.py
+++ Easy/94.BinaryTreeInorderTraversal/main.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def inorderTraversal(self, root):
         """
@@ -21,4 +15,3 @@ class Solution(object):
             if node.right:
                 right_ch = self.inorderTraversal(node.right)
         return left_ch + val_items  + right_ch
-
