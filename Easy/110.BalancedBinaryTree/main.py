dfadab769f45bd7a29fcb274bd9e57156e0479bf
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-03 02:25:05 +0300

    Доделал 110 задачу

--- Easy/110.BalancedBinaryTree/main.py
+++ Easy/110.BalancedBinaryTree/main.py
@@ -10,3 +10,17 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+        def get_h_node(node):
+            if not node:
+                return 0
+            
+            l = 0
+            r = 0
+            if node.left:
+                l = get_h_node(node.left) + 1
+            if node.right:
+                r = get_h_node(node.right) + 1
+            return max(1, max(l, r))
+        if not root:
+            return True
+        return abs(get_h_node(root.left) - get_h_node(root.right)) <= 1
