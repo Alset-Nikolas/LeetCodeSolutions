274acd3072bdf4af41646a7b23659b88f4b27f3e
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-03 11:09:56 +0300

    Начинаю 121 задачу

--- Easy/119.PascalTriangleII/main.py
+++ Easy/119.PascalTriangleII/main.py
@@ -4,3 +4,15 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
+        last_row = []
+        for i in range(rowIndex+1):
+            if i == 0:
+                last_row = [1]
+                continue
+            new_row = [1 for x in range(len(last_row)+1)]
+            for j in range(1, len(new_row)-1):
+                x1 = last_row[j-1]
+                x2 = last_row[j]
+                new_row[j] = x1+x2
+            last_row = new_row
+        return last_row
---
+++ Easy/121.BestTimeBuySellStock/README.md
@@ -0,0 +1,6 @@
+<h1 align="center">Ссылка на задачу
+    <a href='https://leetcode.com/problems/best-time-to-buy-and-sell-stock/'>
+    121. Best Time to Buy and Sell Stock
+    </a>
+</h1>   
+<img src="./info/task.png" height="100%" width="100%"/>
