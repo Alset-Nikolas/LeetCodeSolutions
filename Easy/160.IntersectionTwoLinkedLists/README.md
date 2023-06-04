92a6146b8369604d3bde8b7ea38268c99ef481be
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-04 15:09:42 +0300

    Доделал 160 задачу

---
+++ Easy/160.IntersectionTwoLinkedLists/README.md
@@ -0,0 +1,6 @@
+<h1 align="center">Ссылка на задачу
+    <a href='https://leetcode.com/problems/intersection-of-two-linked-lists/'>
+    160. Intersection of Two Linked Lists
+    </a>
+</h1>   
+<img src="./info/task.png" height="100%" width="100%"/>
---
+++ Easy/160.IntersectionTwoLinkedLists/main.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        items = set()
+        node = headA
+        while node:
+            items.add(node)
+            node = node.next
+        node = headB
+        while node:
+            if node in items:
+                return node
+            node = node.next
+        return 
