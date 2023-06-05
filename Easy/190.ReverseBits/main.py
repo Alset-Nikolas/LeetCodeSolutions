b8d4d4a4f80f175b90bfd88f43146cd6484c8f25
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-05 11:17:13 +0300

    Доделал 190 задачу

---
+++ Easy/190.ReverseBits/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n: int) -> int:
+        n_str = bin(n)[2:]
+        print(n_str, len(n_str))
+
+        return int('0b' + n_str[::-1]+'0'*(32-len(n_str)), 2)
+
+
+    
+if __name__ == "__main__":
+    s = Solution()
+    n = 43261596
+    # n_32b='{:032b}'.format(n)
+    assert s.reverseBits(n) == 964176192
