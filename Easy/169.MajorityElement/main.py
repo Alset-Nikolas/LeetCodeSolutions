cb10ca4593142f05a9f6a5222fc4ec6986d8775b
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-05 10:52:12 +0300

    Доделал 171 задачу

--- Easy/169.MajorityElement/main.py
+++ Easy/169.MajorityElement/main.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        porog = len(nums) // 2
+        numbers_freq = dict()
+        for x in nums:
+            if x not in numbers_freq:
+                numbers_freq[x] = 0
+            numbers_freq[x] += 1
+            if numbers_freq[x] > porog:
+                return x
+        return
---
+++ Easy/171.ExcelSheetColumnNumber/main.py
@@ -0,0 +1,15 @@
+from string import ascii_uppercase
+
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        res = 0
+        for i, x in enumerate(columnTitle[::-1]):
+            res += 26**i*(ascii_uppercase.find(x)+1)
+        return res
+if __name__ == "__main__":
+    s = Solution()
+    print(s.titleToNumber('ZY'))
