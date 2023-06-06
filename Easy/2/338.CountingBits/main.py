d78e57bd9e65ecbe11899cf94c16d45f05ce2365
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-06 16:01:42 +0300

    Доделал 342

--- Easy/2/338.CountingBits/main.py
+++ Easy/2/338.CountingBits/main.py
@@ -1,7 +1,2 @@
-from typing import *
-
-
 class Solution:
-    def countBits(self, n: int) -> List[int]:
-        return [ sum(int(x) for x in bin(x)[2:]) for x in list(range(n))]
-        
+    def isPowerOfFour(self, n: int) -> bool:
