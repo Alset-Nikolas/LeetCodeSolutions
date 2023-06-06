fa1e546d50b4ca0a616969addfe6b233680fda31
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-06 13:25:14 +0300

    доделал 303

---
+++ Easy/2/303.RangeSumQuery-Immutable/README.md
@@ -0,0 +1,6 @@
+<h1 align="center">Ссылка на задачу
+    <a href='https://leetcode.com/problems/range-sum-query-immutable/'>
+  303. Range Sum Query - Immutable
+    </a>
+</h1>   
+<img src="./info/task.png" height="100%" width="100%"/>
---
+++ Easy/2/303.RangeSumQuery-Immutable/main.py
@@ -0,0 +1,19 @@
+from typing import *
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sum_nums = [0]*(len(nums)+1)
+        self.nums = nums
+        self.init_sum()      
+    def init_sum(self):
+        for i, x in enumerate(self.nums):
+            self.sum_nums[i+1] = self.sum_nums[i] + x
+
+    def sumRange(self, left: int, right: int) -> int:
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
