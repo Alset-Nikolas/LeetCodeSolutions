f7db1bc46fc45550106426aa7b5aa8826462ab10
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-06 11:15:24 +0300

    доделал 278 задачу

--- Easy/268.MissingNumber/main.py
+++ Easy/268.MissingNumber/main.py
@@ -1,4 +1,13 @@
 from typing import *
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        pass
+        all_numbers = set()
+        max_ = 0
+        for x in nums:
+            if x > max_:
+                max_ = x
+            all_numbers.add(x)
+        if max_ != len(nums):
+            return max_ + 1
+        x =  set(range(max_)) - set(all_numbers)
+        return x.pop()
---
+++ Easy/278.FirstBadVersion/main.py
@@ -0,0 +1,10 @@
+# The isBadVersion API is already defined for you.
+def isBadVersion(version: int) -> bool:
+    pass
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 1
+        r = 2**31 - 1
+        while l < r:
+            m = (l+r)//2
+            if isBadVersion()
