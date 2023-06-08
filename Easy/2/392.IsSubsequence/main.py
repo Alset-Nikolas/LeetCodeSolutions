dc9a77e34d855c3887302f63faa9594d55d4497c
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-08 13:31:58 +0300

    Доделал 392

---
+++ Easy/2/392.IsSubsequence/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        j = 0
+        while i < len(s) and j < len(t):
+            while j<len(t) and s[i] != t[j]:
+                j += 1
+            if j == len(t):
+                break
+            i += 1
+            j += 1
+
+        return i == len(s)
