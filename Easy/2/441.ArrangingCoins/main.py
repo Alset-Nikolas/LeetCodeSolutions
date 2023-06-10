d5f58882ab3628a3dee2a73472586b612af44e5c
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-10 13:04:23 +0300

    Дописал 441

---
+++ Easy/2/441.ArrangingCoins/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        l = 1
+        r = n 
+        a1 = 1
+        while l <r :
+            m = (l+r+1)//2
+            am = a1+1*(m-1)
+            sum_ = m* (a1 + am)//2
+            if sum_  == n:
+                return m
+            elif sum_ < n:
+                l=m
+            else:
+                r=m-1
+        
+        return l
+
+print(Solution().arrangeCoins(8))
