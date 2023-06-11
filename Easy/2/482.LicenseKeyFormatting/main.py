f948af0ab743bacbc5aafa9cb1ad2e6a38bbb972
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-12 00:17:24 +0300

    Дописал 485

--- Easy/2/482.LicenseKeyFormatting/main.py
+++ Easy/2/482.LicenseKeyFormatting/main.py
@@ -1,15 +1,4 @@
+from typing import * 
 class Solution:
-    def licenseKeyFormatting(self, s: str, k: int) -> str:
-        res = ''
-        l=0
-        for t in s[::-1]:
-            if t == '-':
-                continue
-            if l == k :
-                res = '-' + res
-                l = 0 
-            res = t.upper() + res
-            l += 1
-        return res
-
-print(Solution().licenseKeyFormatting("5F3Z-2e-9-w", 4))
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        nums.append(1)
