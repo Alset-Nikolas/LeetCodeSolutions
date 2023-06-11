318cf88de3f985f3bebdc31e31f9b35e614fbada
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-11 23:57:19 +0300

    Дописал 476

---
+++ Easy/2/476.NumberComplement/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        print(bin((1<<(len(bin(num)[2:]))) - 1))
+
+        return (1<<len(bin(num)[2:])) - 1 - num
+print(Solution().findComplement(1))
