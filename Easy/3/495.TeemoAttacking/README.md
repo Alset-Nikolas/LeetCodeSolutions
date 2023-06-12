107e3721a36a852752897a432b9bab51ad73840f
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-12 12:34:56 +0300

    Дописал 495

---
+++ Easy/3/495.TeemoAttacking/README.md
@@ -0,0 +1,6 @@
+<h1 align="center">Ссылка на задачу
+    <a href='https://leetcode.com/problems/teemo-attacking/'>
+    495. Teemo Attacking
+    </a>
+</h1>   
+<img src="./info/task.png" height="100%" width="100%"/>
---
+++ Easy/3/495.TeemoAttacking/main.py
@@ -0,0 +1,24 @@
+from typing import * 
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        if not timeSeries:
+            return 0
+        res = 0
+        r_last = None
+        for i in range(len(timeSeries)):
+            li = timeSeries[i]
+            if r_last is None:
+                r_last = li + duration
+                continue
+            if li > r_last:
+                res += duration
+            else:
+                delta = li - (r_last - duration)
+                res += delta
+            r_last = li + duration
+
+
+
+        res += duration
+        return res
+            
