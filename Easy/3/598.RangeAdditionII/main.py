21fd76c91a01b1880a14f7046a1d34fbec020ec3
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-15 14:40:43 +0300

    Доделал 598

--- Easy/3/598.RangeAdditionII/main.py
+++ Easy/3/598.RangeAdditionII/main.py
@@ -2,20 +2,6 @@ from typing import *
 # Definition for a Node.
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        matr = [[0 for y in range(m)] for x in range(n)]
-        res = 0
-        res_max = 0
-        for x, y in ops:
-            for yj in range(y):
-                for xi in range(x):
-                    matr[xi][yj] +=1
-                    new_variant = matr[xi][yj]
-                    if res_max < new_variant:
-                        res_max = new_variant
-                        res = 1
-                    elif new_variant == res_max:
-                        res += 1
-
-
-        return res
-print(Solution().maxCount(3, 3, [[2,2],[3,3]]))
+        if ops == []:
+            return m*n
+        return min(x for x,y in ops) * min(y for x,y in ops) 
