ebba826ca3ff0fc5f0c8ab7d6b4fb70bd88f3715
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-15 11:01:45 +0300

    Доделал 566.ReshapeMatrix

---
+++ Easy/3/566.ReshapeMatrix/README.md
@@ -0,0 +1,6 @@
+<h1 align="center">Ссылка на задачу
+    <a href='https://leetcode.com/problems/reshape-the-matrix/'>
+   566. Reshape the Matrix
+    </a>
+</h1>   
+<img src="./info/task.png" height="100%" width="100%"/>
---
+++ Easy/3/566.ReshapeMatrix/main.py
@@ -0,0 +1,22 @@
+from typing import *
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:        
+        size_line = len(mat)
+        size_clmn = len(mat[0])
+        if r*c != size_line*size_clmn:
+            return mat
+        
+        i_line = 0
+        j_clmn = 0
+        new_mat = []
+        for i in range(r):
+            new_line = []
+            for j in range(c):
+                new_line.append(mat[i_line][j_clmn])
+                
+                j_clmn =(j_clmn + 1)%size_clmn
+                if j_clmn == 0:
+                    i_line = (i_line + 1)%size_line
+            new_mat.append(new_line)
+        return new_mat
