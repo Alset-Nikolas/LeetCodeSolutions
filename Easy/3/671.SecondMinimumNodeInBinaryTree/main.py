5acf1a084ddccc97725ec2cfddecf52de618ca64
Николай Кириллович <Alset.Nikolas@yandex.ru>
2023-06-19 18:18:27 +0300

    Доделал 682

--- Easy/3/671.SecondMinimumNodeInBinaryTree/main.py
+++ Easy/3/671.SecondMinimumNodeInBinaryTree/main.py
@@ -1,30 +1,22 @@
 from typing import * 
 
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
-    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
-        def dfs(node: Optional[TreeNode], val_root:int) -> int:
-            if not node:
-                return -1
-            if not node.left:
-                return -1
-       
-            if node.left.val == val_root:
-                l = self.findSecondMinimumValue(node.left)   
-            else:
-                l = node.left.val           
-            if node.right.val == val_root:
-                r = self.findSecondMinimumValue(node.right)
-            else:
-                r = node.right.val
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        if nums == []:
+            return 0
+        l = 0
 
-            return min(l, r) if l != -1 and r!=-1 else max(l, r)
-        return dfs(root, root.val)
+        lwin = 0
+        rwin = 1
+        nums.append(-1)
+        for r in range(1, len(nums)):
+            if nums[r-1] >= nums[r]:
+                if r-l > rwin - lwin:
+                    lwin=l
+                    rwin = r
+                l=r
+        return rwin - lwin
+
+
             
-        
---
+++ Easy/3/682.BaseballGame/main.py
@@ -0,0 +1,38 @@
+from typing import * 
+class Stack:
+    def __init__(self) -> None:
+        self.mass = []
+
+    def add(self, x):
+        try:
+            self.mass.append(int(x))
+        except:
+            self.mass.append(x)
+
+    def pop(self):
+        return self.mass.pop()
+    
+    def is_empty(self):
+        return self.mass == []
+    
+    def check(self):
+        return self.mass[-1]
+    def sum(self):
+        self.mass.append(self.mass[-1] + self.mass[-2])
+    def x2(self):
+        self.mass.append(self.mass[-1]*2)
+    
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        s = Stack()
+        for x in operations:
+            if x not in ["C", "D", "+"]:
+                s.add(x)
+            elif x == 'C':
+                s.pop()
+            elif x == 'D':
+                s.x2()
+            elif x == '+':
+                s.sum()
+        return sum(s.mass) if s.mass else 0  
